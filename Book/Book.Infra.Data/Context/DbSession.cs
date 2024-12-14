b3dc7aebb4922fbfc620603cc1993351c298dfb2
Gabriel Ferreira <gabrielferreirasouto.com@hotmail.com>
2024-12-14 20:16:12 -0300

    feat: configurando a conexão com o banco de dados

---
+++ Book/Book.Infra.Data/Context/DbSession.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+
+namespace Book.Infra.Data.Context
+{
+    public class DbSession : IDisposable
+    {
+        public IDbConnection Connection { get; set; }
+        public DbSession(IConfiguration configuration) 
+        {
+            Connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+            Connection.Open();
+        }
+
+        public void Dispose() => Connection?.Dispose();
+        
+    }
+}
---
+++ Book/Book.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,17 @@
+﻿
+using Book.Infra.Data.Context;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjectionAPI
+{
+    public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services,
+    IConfiguration configuration)
+    {
+
+
+        services.AddScoped<DbSession>();
+        var myhandlers = configuration.GetSection("Book.Application");
+        return services;
+    }
+}
