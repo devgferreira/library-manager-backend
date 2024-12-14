06ee766e5049a9c4ef70427e3703e934f1332f48
Gabriel Ferreira <gabrielferreirasouto.com@hotmail.com>
2024-12-14 20:18:04 -0300

    feat: criando o repository Book

---
+++ Book/Book.Infra.Data/Repository/BookRepository.cs
@@ -0,0 +1,42 @@
+﻿using Book.Domain.Contracts;
+using Book.Domain.Entities.Book;
+using Book.Domain.Entities.Book.Request;
+using Book.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.Infra.Data.Repository
+{
+    public class BookRepository : IBookRepository
+    {
+        private DbSession _session;
+
+        public BookRepository(DbSession session)
+        {
+            _session = session;
+        }
+
+        public Task DeleteAsync(int bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BookInfo> InsertAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BookInfo> SelectBook(BookRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BookInfo> UpdateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
