399818170364815ffae1cba3faa42f0c0c32326d
Gabriel Ferreira <gabrielferreirasouto.com@hotmail.com>
2024-12-13 23:31:00 -0300

    feat: criando os contratos a serem utilizados

---
+++ Book/Book.Domain/Contracts/IBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Book.Domain;
+using Book.Domain.Entities;
+
+namespace Book.Domain.Contracts
+{
+    public interface IBookRepository
+    {
+        Task<BookInfo> InsertAsync();
+        Task<BookInfo> UpdateAsync();
+        Task DeleteAsync(int bookId);
+        Task<BookInfo> SelectBook(string title, string author, string genre, string isbn);
+    }
+}
